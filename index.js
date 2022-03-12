8a7cd7cc112416c73a9dd91938ab339e49bdf6f1
Ignacio Bergantiños <bergantinos.ignacio@gmail.com>
2022-03-12 17:20:00 -0300

    Added clear freatures

--- index.js
+++ index.js
@@ -22,28 +22,50 @@ function showResult(result) {
 }
 
 function del() {
-    let newstring = container.value.slice(0, -1);
-    container.value = newstring;
+    if (container.value === "Please enter a number") {
+        container.value = "";
+        let newstring = container.value.slice(0, -1);
+        container.value = newstring;
+    } else {
+        let newstring = container.value.slice(0, -1);
+        container.value = newstring;
+    }
 }
 function clear() {
     container.value = "";
 }
 
+function clearOnClick() {
+    if (container.value === "Please enter a number") {
+        clear();
+    }
+}
 const numberButtons = document.querySelectorAll("[data-numbers]");
 const operationButtons = document.querySelectorAll("[data-operations]");
 
 numberButtons.forEach((button) => {
     button.addEventListener("click", () => {
-        container.value += button.innerText;
+        if (container.value === "Please enter a number") {
+            container.value = "";
+            container.value += button.innerText;
+        } else {
+            container.value += button.innerText;
+        }
     });
 });
 
 operationButtons.forEach((button) => {
     button.addEventListener("click", () => {
-        container.value += button.innerText;
+        if (container.value === "Please enter a number") {
+            container.value = "";
+            container.value += button.innerText;
+        } else {
+            container.value += button.innerText;
+        }
     });
 });
 
+container.addEventListener("click", clearOnClick);
 document.getElementById("del").addEventListener("click", del);
 document.getElementById("ac").addEventListener("click", clear);
 document.getElementById("equal").addEventListener("click", mathOperation);
