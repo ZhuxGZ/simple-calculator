5bb03b00327549a24721c9707d09e3aa6d898517
Ignacio Bergantiños <bergantinos.ignacio@gmail.com>
2022-03-12 14:44:53 -0300

    Added Buttons and a little of css

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-function mathOperation() {
-    let display = document.getElementById("display").value;
+let container = document.getElementById("display");
 
-    if (display.length == 0) {
+function mathOperation() {
+    if (container.value.length == 0) {
         result = "Please enter a number";
     } else {
-        result = eval(display);
+        result = eval(container.value);
     }
 
     showResult(result);
 }
 
 function showResult(result) {
-    let container = document.getElementById("display");
     container.value = result;
 
     if (document.getElementById("result") !== null) {
@@ -22,4 +21,29 @@ function showResult(result) {
     }
 }
 
-document.getElementById("calculate").addEventListener("click", mathOperation);
+function del() {
+    let newstring = container.value.slice(0, -1);
+    container.value = newstring;
+}
+function clear() {
+    container.value = "";
+}
+
+const numberButtons = document.querySelectorAll("[data-numbers]");
+const operationButtons = document.querySelectorAll("[data-operations]");
+
+numberButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        container.value += button.innerText;
+    });
+});
+
+operationButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        container.value += button.innerText;
+    });
+});
+
+document.getElementById("del").addEventListener("click", del);
+document.getElementById("ac").addEventListener("click", clear);
+document.getElementById("equal").addEventListener("click", mathOperation);
